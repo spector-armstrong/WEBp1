f859650cba18ff4d4d8877e07c7bfc76c51811e6
spector-armstrong <franklinluvv@gmail.com>
2026-02-07 20:33:26 +0300

    added modal window for checkout

---
+++ script.js
@@ -0,0 +1,19 @@
+let modal = document.querySelector(".modal");
+
+let btn = document.querySelector(".cart-btn");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
